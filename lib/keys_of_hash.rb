2379294739b1a85f538230481732f4de217074a7
AHAMED ABBAS <ahamedali95@gmail.com>
2018-03-22 01:02:35 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,13 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    keys = []
+
+    Hash.each do |key, value|
+      if value == arguments
+        keys << key
+      end
+   end
   end
+  
+  keys
 end
